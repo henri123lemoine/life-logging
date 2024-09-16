9928743887c2b23830894520f0d4f239fced2ebe
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-16 14:18:41 -0400

    Fix for async config

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -7,11 +7,12 @@ use crate::app_state::AppState;
 use crate::error::{LifeLoggingError, Result};
 
 pub async fn run_server(app_state: &Arc<AppState>) -> Result<()> {
-    let app = routes::create_router(app_state.clone());
+    let app: axum::Router = routes::create_router(app_state.clone());
 
+    let config = app_state.config_manager.get_config().await;
     let addr = SocketAddr::new(
-        app_state.config.server.host.parse().map_err(|e| LifeLoggingError::ServerError(format!("Invalid host: {}", e)))?,
-        app_state.config.server.port
+        config.server.host.parse().map_err(|e| LifeLoggingError::ServerError(format!("Invalid host: {}", e)))?,
+        config.server.port
     );
     tracing::info!("Listening on {}", addr);
     axum_server::bind(addr)
