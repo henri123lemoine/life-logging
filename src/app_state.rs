fabbfcd7563c6dd295104f448ebd927487bab753
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-16 14:17:44 -0400

    Changed AppState to use config manager

--- src/app_state.rs
+++ src/app_state.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 use std::time::SystemTime;
 use tokio::sync::broadcast;
 use crate::audio::buffer::CircularAudioBuffer;
-use crate::config::Config;
+use crate::config::ConfigManager;
 use crate::error::Result;
 use crate::audio::encoder::EncoderFactory;
 
@@ -10,21 +10,24 @@ pub struct AppState {
     pub audio_buffer: Arc<CircularAudioBuffer>,
     pub audio_sender: broadcast::Sender<Vec<f32>>,
     pub start_time: SystemTime,
-    pub config: Arc<Config>,
+    pub config_manager: Arc<ConfigManager>,
     pub encoder_factory: EncoderFactory,
 }
 
 impl AppState {
-    pub fn new(config: &Arc<Config>) -> Result<Arc<Self>> {
-        let buffer_size = config.sample_rate as usize * config.buffer_duration as usize;
-        let audio_buffer = Arc::new(CircularAudioBuffer::new(buffer_size, config.sample_rate));
-        let (audio_sender, _) = broadcast::channel(1024);
+    pub async fn new(config_manager: &Arc<ConfigManager>) -> Result<Arc<Self>> {
+        let config = config_manager.get_config().await;
+        let (_, stream_config) = config_manager.get_audio_config().await?;
+        let buffer_size = config.buffer_duration as usize * stream_config.sample_rate.0 as usize;
+        let audio_buffer = Arc::new(CircularAudioBuffer::new(buffer_size, stream_config.sample_rate.0));
+        let buffer_size = config.audio_channel_buffer_size.unwrap_or(1024);
+        let (audio_sender, _) = broadcast::channel(buffer_size);
 
         let app_state = Arc::new(AppState {
             audio_buffer: audio_buffer.clone(),
             audio_sender,
             start_time: SystemTime::now(),
-            config: config.clone(),
+            config_manager: config_manager.clone(),
             encoder_factory: EncoderFactory::new(),
         });
 
