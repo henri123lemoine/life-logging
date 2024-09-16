6bc128617572d51e1a729b528c6f7d0f53241e82
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-16 14:17:59 -0400

    main.rs loads the config manager

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,20 @@ mod server;
 mod app_state;
 mod error;
 
+use std::sync::Arc;
 use app_state::AppState;
 use audio::processor;
 use error::Result;
+use config::ConfigManager;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     tracing::info!("Starting Life-Logging audio recording service");
 
-    let config = config::load_config()?;
-    let app_state = AppState::new(&config)?;
-    processor::setup_audio_processing(&app_state)?;
+    let config_manager = Arc::new(ConfigManager::new()?);
+    let app_state = AppState::new(&config_manager).await?;
+    processor::setup_audio_processing(&app_state).await?;
     
     server::run_server(&app_state).await?;
     Ok(())
