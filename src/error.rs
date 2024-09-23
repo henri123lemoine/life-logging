cfb8b4e05fb5b2e9d921239b256f8d11a41e291a
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-22 23:41:38 -0400

    Renamed errors according to clippy suggestions

--- src/error.rs
+++ src/error.rs
@@ -1,49 +1,42 @@
 use thiserror::Error;
-use cpal::DevicesError;
-use cpal::SupportedStreamConfigsError;
-use cpal::DefaultStreamConfigError;
-use cpal::DeviceNameError;
 
 #[derive(Error, Debug)]
 pub enum LifeLoggingError {
     #[error("Configuration error: {0}")]
-    ConfigError(#[from] config::ConfigError),
+    Config(#[from] config::ConfigError),
 
     #[error("Audio device error: {0}")]
-    AudioDeviceError(String),
+    AudioDevice(String),
 
     #[error("Audio stream error: {0}")]
-    AudioStreamError(#[from] cpal::BuildStreamError),
+    AudioStream(#[from] cpal::BuildStreamError),
 
     #[error("Audio stream play error: {0}")]
-    AudioStreamPlayError(#[from] cpal::PlayStreamError),
+    AudioStreamPlay(#[from] cpal::PlayStreamError),
 
     #[error("Encoding error: {0}")]
-    EncodingError(String),
+    Encoding(String),
 
     #[error("I/O error: {0}")]
-    IoError(#[from] std::io::Error),
+    Io(#[from] std::io::Error),
 
     #[error("Server error: {0}")]
-    ServerError(String),
+    Server(String),
 
     #[error("Axum error: {0}")]
-    AxumError(#[from] axum::Error),
+    Axum(#[from] axum::Error),
 
     #[error("Device enumeration error: {0}")]
-    DevicesError(#[from] DevicesError),
+    Devices(#[from] cpal::DevicesError),
 
     #[error("Supported stream configs error: {0}")]
-    SupportedStreamConfigsError(#[from] SupportedStreamConfigsError),
+    SupportedStreamConfigs(#[from] cpal::SupportedStreamConfigsError),
 
     #[error("Default stream config error: {0}")]
-    DefaultStreamConfigError(#[from] DefaultStreamConfigError),
+    DefaultStreamConfig(#[from] cpal::DefaultStreamConfigError),
 
     #[error("Device name error: {0}")]
-    DeviceNameError(#[from] DeviceNameError),
-
-    // #[error("Audio processing error: {0}")]
-    // AudioProcessingError(String),
+    DeviceName(#[from] cpal::DeviceNameError),
 }
 
 pub type Result<T> = std::result::Result<T, LifeLoggingError>;
