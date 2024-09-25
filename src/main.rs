6dba9bf744e4fa5f256310254a008e598defeae2
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-24 23:19:30 -0400

    Combined persistence and audio cleanup in main

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,22 @@ use life_logging::app_state::AppState;
 use life_logging::audio::processor;
 use life_logging::error::Result;
 use life_logging::server;
-use std::time::Duration;
+use std::sync::Arc;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     tracing::info!("Starting Life-Logging audio recording service");
 
-    let app_state = AppState::new().await?;
+    let app_state = Arc::new(AppState::new().await?);
     processor::setup_audio_processing(&app_state).await?;
 
     // Start the persistence task
-    tokio::spawn({
-        let app_state = app_state.clone();
-        async move {
-            app_state
-                .disk_storage
-                .start_persistence_task(app_state.audio_buffer.clone())
-                .await;
-        }
-    });
-
-    // Start the cleanup task
-    tokio::spawn({
-        let disk_storage = app_state.disk_storage.clone();
-        async move {
-            disk_storage
-                .start_cleanup_task(
-                    Duration::from_secs(60 * 60),
-                    Duration::from_secs(60 * 60 * 24),
-                )
-                .await;
-        }
+    let persistence_app_state = app_state.clone();
+    tokio::spawn(async move {
+        let disk_storage = persistence_app_state.disk_storage.clone();
+        let audio_buffer = persistence_app_state.audio_buffer.clone();
+        disk_storage.start_persistence_task(audio_buffer).await;
     });
 
     server::run_server(&app_state).await?;
