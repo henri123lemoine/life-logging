fa608510e8c50e39399691d708cf4cb17e41ee4c
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-26 03:25:07 -0400

    Added recent modules to persistence's mod.rs

--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,2 +1,10 @@
-mod disk_storage;
+mod local_storage;
+mod s3_storage;
+mod storage;
+mod storage_manager;
+
 pub use disk_storage::DiskStorage;
+pub use local_storage::LocalStorage;
+pub use s3_storage::S3Storage;
+pub use storage::Storage;
+pub use storage_manager::StorageManager;
